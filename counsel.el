c9a094c5fdc5c53b07d45ceddec546a5ce64369d
Madhu <enometh@net.meer>
2020-08-13 15:46:20 +0530

    ivy.el: ivy-switch-to-buffer ala iswitchb

--- counsel.el
+++ counsel.el
@@ -6664,7 +6664,7 @@ The buffers are those opened during a session of `counsel-switch-buffer'."
            (message ""))))
       (t
        (with-ivy-window
-         (switch-to-buffer (ivy-state-buffer ivy-last)))))))
+         (ivy-switch-to-buffer (ivy-state-buffer ivy-last)))))))
 
 ;;;###autoload
 (defun counsel-switch-buffer ()
