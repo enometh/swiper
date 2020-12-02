d72e1957f5f9804d73595eabdced11c06c696507
Madhu <enometh@net.meer>
2020-12-02 14:53:49 +0530

    ivy.el: (ivy-insert-current-full) - don't fail if not reading a directory

--- ivy.el
+++ ivy.el
@@ -5086,7 +5086,14 @@ Don't finish completion."
 (defun ivy-insert-current-full ()
   "Insert the current directory into the minibuffer."
   (interactive)
-  (insert ivy--directory))
+  (if ivy--directory
+      (insert ivy--directory)
+    (delete-minibuffer-contents)
+    (let ((end (and ivy--directory
+                  (ivy--dirname-p (ivy-state-current ivy-last))
+                  -1)))
+    (insert (substring-no-properties
+             (ivy-state-current ivy-last) 0 end)))))
 (ivy--no-M-x #'ivy-insert-current-full #'ivy--minibuffer-p)
 
 (defcustom ivy-preferred-re-builders
